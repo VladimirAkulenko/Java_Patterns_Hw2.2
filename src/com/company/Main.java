96d36fa387b94bb99eb6ec895faed572a55a3a30
Vladimir Akulenko <vladimirakulenk@yandex.ru>
2023-12-16 17:33:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1010", "1010"));
+        System.out.println(bins.mult("1010", "1010"));
+    }
+}
